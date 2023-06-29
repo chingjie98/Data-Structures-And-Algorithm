6f8de5ef886b44164b92c4404a1fc743c441d3b5
chingjie98 <35895182+chingjie98@users.noreply.github.com>
2023-06-29 15:34:25 +0800

    Completed code for static Array to become Dynamic Array

--- Arrays/src/Array.java
+++ Arrays/src/Array.java
@@ -1,2 +1,67 @@
 public class Array {
+
+    private int[] items;
+    private int count;
+
+    public Array(int length){
+        this.items = new int[length];
+        this.count = 0;
+    }
+
+    public void Print(){
+        for (int i=0; i<count; i++) {
+            System.out.println(items[i]);
+        }
+    }
+
+    public void Insert(int item){
+
+        if (count == items.length){
+            // create a new array if the size is maxed out
+            // and set that new array to be the current array
+            int newLength = count * 2;
+            int[] newItems = new int[newLength];
+
+            for (int i=0; i<count; i++){
+                newItems[i] = items[i];
+            }
+            items = newItems;
+        }
+        items[count] = item;
+        count++;
+    }
+
+    public void RemoveAt(int index){
+        //if index is 1, remove item at index 1, and shift all items from right to left
+        //to shift items from right to left, start with the existing count at index
+        //[10,20,30,40]
+        //[10,30,40]
+        if (index < 0 || index >= count){
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = index; i < count; i++){
+
+            if (i < count - 1) {
+                items[i] = items[i + 1];
+            }
+        }
+
+        count--;
+    }
+
+    public int IndexOf(int value){
+
+        for (int i = 0; i < count; i++){
+            if (items[i] == value){
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
+
+
+
 }
--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        int[] numbers = new int[3];
+//        numbers[0] = 5;
+//        System.out.println(Arrays.toString(numbers));
+
+        Array num1 = new Array(2);
+
+        num1.Insert(5);
+        num1.Insert(8);
+        num1.Insert(9);
+        num1.Insert(15);
+        num1.Insert(9);
+
+
+        System.out.println(num1.IndexOf(9));
     }
 }
